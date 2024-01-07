688a3418ecea0507a44e50bb29557d9d10cbbc91
jeffrey-spaan <jeffrey-spaan@users.noreply.github.com>
2024-01-07 16:31:27 +0100

    Initial commit

---
+++ src/main/java/com/jeffreyspaan/security/book/BookService.java
@@ -0,0 +1,25 @@
+package com.jeffreyspaan.security.book;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookRepository repository;
+
+    public void save(BookRequest request) {
+        var book = Book.builder()
+                .author(request.getAuthor())
+                .isbn(request.getIsbn())
+                .build();
+        repository.save(book);
+    }
+
+    public List<Book> findAll() {
+        return repository.findAll();
+    }
+}
---
+++ src/main/java/com/jeffreyspaan/security/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.jeffreyspaan.security.token;
+
+public enum TokenType {
+    BEARER
+}
